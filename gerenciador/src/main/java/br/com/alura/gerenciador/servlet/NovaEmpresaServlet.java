b18fb8f32167a4aecd15a1d12ae2cc9c8a1422d8
lucas oliveira <lucas.orodrigues@hotmail.com>
2022-11-17 18:51:14 -0300

    redirecionamento-pelo-navegador

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -41,10 +41,13 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		//chama o JSP
-		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
 		request.setAttribute("empresa", empresa.getNome());	
-		rd.forward(request, response);
+		
+		response.sendRedirect("listaEmpresas");
+		
+		//chama o JSP
+//		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
+//		rd.forward(request, response);
 			
 	}
 
